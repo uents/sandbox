be3cbe811b2b2bb05393e0950b77a2e865ad7d96
Takamasa Ueno <uents@ymail.com>
2016-11-22 08:57:16 +0900

    テンプレートリテラルのテストコードを追加

---
+++ ecmascript6/es5/template-literal.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// 変数埋め込み
+var name = "Bob";
+console.log("Hello. " + name);
+
+// 複数行文字列
+var text = "line1\nline2\nline3\n";
+console.log(text);
+
+// タグ付きテンプレート
+// TODO: elって何？
+var name = "Bob <script>";
+//el.innerHTML = html`<p>Hello, ${name}</p>`;
