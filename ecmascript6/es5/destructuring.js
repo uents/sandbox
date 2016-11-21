d6ed4db66566f9a65d002326657f64e51ab77603
Takamasa Ueno <uents@ymail.com>
2016-11-22 07:03:49 +0900

    分割代入のテストコードを追加

---
+++ ecmascript6/es5/destructuring.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
+// 配列の分割代入
+var year = 2016,
+    month = 12,
+    day = 31;
+
+console.log(year, month, day);
+
+// さらに2番目の値だけを代入
+var _ref = [2015, 1, 1],
+    month = _ref[1];
+
+console.log(year, month, day);
+
+// レストパラメータの活用
+var year = 2014,
+    monthDay = [4, 1];
+
+console.log(year, monthDay);
+
+// 値の交換に便利
+var x = 1,
+    y = 2;
+var _ref2 = [y, x];
+x = _ref2[0];
+y = _ref2[1];
+
+console.log(x, y);
+
+// オブジェクトの分割代入
+var _name$age = { name: "Bob", age: 20 },
+    a = _name$age.name,
+    b = _name$age.age;
+
+console.log(a, b);
+
+// プロパティ省略記法を使った分割代入
+var _name$age2 = { name: "John", age: 20 },
+    name = _name$age2.name,
+    age = _name$age2.age;
+
+console.log(name, age);
+
+// デフォルト値の設定
+var _name = { name: "Tom" },
+    name = _name.name,
+    _name$age3 = _name.age,
+    age = _name$age3 === undefined ? 18 : _name$age3;
+
+console.log(name, age);
+
+// ネストしたオブジェクトからの抽出
+
+var _foo = { foo: { bar: [1, 2, 3] } },
+    _foo$foo$bar = _slicedToArray(_foo.foo.bar, 2),
+    x = _foo$foo$bar[1];
+
+console.log(x);
---
+++ ecmascript6/es6/destructuring.js
@@ -0,0 +1,33 @@
+// 配列の分割代入
+var [year, month, day] = [2016, 12, 31];
+console.log(year, month, day);
+
+// さらに2番目の値だけを代入
+var [, month] = [2015, 1, 1];
+console.log(year, month, day);
+
+// レストパラメータの活用
+var [year, ...monthDay] = [2014, 4, 1];
+console.log(year, monthDay);
+
+// 値の交換に便利
+var x = 1,
+    y = 2;
+[x, y] = [y, x];
+console.log(x, y);
+
+// オブジェクトの分割代入
+var {name: a, age: b} = {name: "Bob", age: 20};
+console.log(a, b);
+
+// プロパティ省略記法を使った分割代入
+var {name, age} = {name: "John", age: 20};
+console.log(name, age);
+
+// デフォルト値の設定
+var {name, age = 18} = {name: "Tom"};
+console.log(name, age);
+
+// ネストしたオブジェクトからの抽出
+var {foo: {bar: [, x]}} = {foo: {bar: [1, 2, 3]}}
+console.log(x);
